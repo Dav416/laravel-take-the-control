16c40bb2b9143919403f01e90078b4940c7ca149
Dav416 <84945215+Dav416@users.noreply.github.com>
2025-09-27 18:34:19 -0500

    styles(views, dashboard): add error alert style

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,6 +19,10 @@
         <div class="alert alert-success">{{ session('success') }}</div>
     @endif
 
+    @if(session('error'))
+        <div class="alert alert-danger">{{ session('error') }}</div>
+    @endif
+
     <div class="card shadow">
         <div class="card-body">
             <table class="table table-bordered table-hover">
